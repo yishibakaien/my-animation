7ceb6f6e9aa2d7110c07e1b8f8e2437301527095
yishibakaien <646932161@qq.com>
2021-07-27 17:43:52 +0800

    feat: bugfix and format code

--- src/animation.js
+++ src/animation.js
@@ -1,53 +1,52 @@
-'use strict';
-var Timeline = require('./timeline.js');
-var loadImage = require('./imageloader.js');
+'use strict'
+var Timeline = require('./timeline.js')
+var loadImage = require('./imageloader.js')
 
 /**
  * animation执行状态定义
  */
 // 初始化状态
-var STATE_INITIAL = 0;
+var STATE_INITIAL = 0
 // 开始状态
-var STATE_START = 1;
+var STATE_START = 1
 // 停止状态
-var STATE_STOP = 2;
+var STATE_STOP = 2
 
 /**
  * 任务链类型定义（两种：同步、异步）
  */
 // 同步任务
-var TASK_SYNC = 0;
+var TASK_SYNC = 0
 // 异步任务
-var TASK_ASYNC = 1;
+var TASK_ASYNC = 1
 
 /**
  * 默认异步任务执行间隔时间
  */
-var DEFAULT_INTERVAL = 80;
+var DEFAULT_INTERVAL = 80
 
 /**
  * 简单的函数包装，执行callback
  * @param  {Function} callback 执行的函数
  */
 function next(callback) {
-    callback && callback();
+  callback && callback()
 }
 
 /**
  * [Animation description] 帧动画库类
  */
 function Animation() {
-    // 任务链
-    this.taskQueue = [];
+  // 任务链
+  this.taskQueue = []
 
-    this.timeline = new Timeline();
+  this.timeline = new Timeline()
 
-    // animation 的状态
-    this.state = STATE_INITIAL;
+  // animation 的状态
+  this.state = STATE_INITIAL
 
-    // ！！当前任务索引 （用于维护任务链的索引值）
-    this.index = 0;
-
+  // ！！当前任务索引 （用于维护任务链的索引值）
+  this.index = 0
 }
 
 /**
@@ -58,16 +57,16 @@ function Animation() {
  * @return {[type]}         返回this._add()
  */
 Animation.prototype.loadImage = function (imgList) {
-    var taskFn = function (next) {
-        // slice() 深拷贝数组，创建一个imgList 副本，
-        // 对副本进行操作，避免影响原数组
-        // ！！next 作为回调函数，
-        // 传给 loadImage方法，(即loadImage中的第二个参数 callback)
-        loadImage(imgList.slice(), next);
-    }
-    var type = TASK_SYNC;
+  var taskFn = function (next) {
+    // slice() 深拷贝数组，创建一个imgList 副本，
+    // 对副本进行操作，避免影响原数组
+    // ！！next 作为回调函数，
+    // 传给 loadImage方法，(即loadImage中的第二个参数 callback)
+    loadImage(imgList.slice(), next)
+  }
+  var type = TASK_SYNC
 
-    return this._add(taskFn, type);
+  return this._add(taskFn, type)
 }
 
 /**
@@ -78,50 +77,49 @@ Animation.prototype.loadImage = function (imgList) {
  * @param  {[type]} imageUrl  图片地址
  * @return {[type]}           返回this._add();
  */
-Animation.prototype.changePosition = function(ele, positions, imageUrl) {
-    var len = positions.length;
-    var taskFn;
-    var type;
+Animation.prototype.changePosition = function (ele, positions, imageUrl) {
+  var len = positions.length
+  var taskFn
+  var type
 
-    // 如果有长度，则是一个真正的异步定时任务
-    if (len) {
-        var _this = this;
-        /**
-         * [taskFn description]
-         * @param  {Function} next 帧动画执行完后执行的回调方法
-         * @param  {[type]}   time 帧动画开始到现在执行的时间
-         * @return {[type]}        [description]
-         */
-        taskFn = function (next, time) {
-            if (imageUrl) {
-                ele.style.backgroundImage = 'url(' + imageUrl + ')';
+  // 如果有长度，则是一个真正的异步定时任务
+  if (len) {
+    var _this = this
+    /**
+     * [taskFn description]
+     * @param  {Function} next 帧动画执行完后执行的回调方法
+     * @param  {[type]}   time 帧动画开始到现在执行的时间
+     * @return {[type]}        [description]
+     */
+    taskFn = function (next, time) {
+      if (imageUrl) {
+        ele.style.backgroundImage = 'url(' + imageUrl + ')'
+      }
+      // 获得当前图片背景位置索引
+      // | 0 : 相当于Math.floor，相率更高
+      // len : 由于 time可能很大，所以规定要小于 len，
+      // 也就是说 索引值不能超过len
+      var index = Math.min((time / _this.interval) | 0, len)
+      var position = positions[index - 1].split(' ')
+      // 改变dom对象的背景图片位置
+      ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px'
 
-            }
-            // 获得当前图片背景位置索引
-            // | 0 : 相当于Math.floor，相率更高
-            // len : 由于 time可能很大，所以规定要小于 len，
-            // 也就是说 索引值不能超过len
-            var index = Math.min(time / _this.interval | 0, len);
-            var position = positions[index - 1].split(' ');
-            // 改变dom对象的背景图片位置
-            ele.style.backgroundPosition = position[0] + 'px ' + position[1] + 'px';
-
-            // 当动画进行到最后一帧时
-            if (index === len) {
-                next();
-            }
-        }
-        type = TASK_ASYNC;
+      // 当动画进行到最后一帧时
+      if (index === len) {
+        next()
+      }
     }
-    // 如果没有长度
-    else {
-        // 全局的next 方法
-        taskFn = next;
-        type = TASK_SYNC;
-    }
+    type = TASK_ASYNC
+  }
+  // 如果没有长度
+  else {
+    // 全局的next 方法
+    taskFn = next
+    type = TASK_SYNC
+  }
 
-    // 将 taskFn 和 type 添加到任务队列，并返回出去
-    return this._add(taskFn, type);
+  // 将 taskFn 和 type 添加到任务队列，并返回出去
+  return this._add(taskFn, type)
 }
 
 /**
@@ -132,29 +130,28 @@ Animation.prototype.changePosition = function(ele, positions, imageUrl) {
  * @return {[type]}         返回this._add()
  */
 Animation.prototype.changeSrc = function (ele, imgList) {
-    var len = imgList.length;
-    var taskFn;
-    var type;
+  var len = imgList.length
+  var taskFn
+  var type
 
-    if (len) {
-        var _this = this;
-        taskFn = function (next, time) {
-            // 获得当前图片索引
-            var index = Math.min(time / _this.interval | 0, len);
-            // 改变image对象的图片地址
-            ele.src = imgList[index - 1];
-            if (index === len) {
-                next();
-            } 
-        }
-        type = TASK_ASYNC;
+  if (len) {
+    var _this = this
+    taskFn = function (next, time) {
+      // 获得当前图片索引
+      var index = Math.min((time / _this.interval) | 0, len)
+      // 改变image对象的图片地址
+      ele.src = imgList[index - 1]
+      if (index === len) {
+        next()
+      }
     }
-    else {
-        taskFn = next;
-        type = TASK_SYNC;
-    }
+    type = TASK_ASYNC
+  } else {
+    taskFn = next
+    type = TASK_SYNC
+  }
 
-    return this._add(taskFn, type);
+  return this._add(taskFn, type)
 }
 
 /**
@@ -164,7 +161,7 @@ Animation.prototype.changeSrc = function (ele, imgList) {
  * @return {[type]}        返回this._add()
  */
 Animation.prototype.enterFrame = function (taskFn) {
-    return this._add(taskFn, TASK_ASYNC);
+  return this._add(taskFn, TASK_ASYNC)
 }
 
 /**
@@ -174,14 +171,13 @@ Animation.prototype.enterFrame = function (taskFn) {
  * @return {[type]}            返回this._add()
  */
 Animation.prototype.then = function (callback) {
-    
-    // then 是一个同步任务，只需要接收一个参数 next
-    var taskFn = function (next) {
-        callback();
-        next();
-    }
-    var type = TASK_SYNC;
-    return this._add(taskFn, type);
+  // then 是一个同步任务，只需要接收一个参数 next
+  var taskFn = function (next) {
+    callback()
+    next()
+  }
+  var type = TASK_SYNC
+  return this._add(taskFn, type)
 }
 
 /**
@@ -190,26 +186,26 @@ Animation.prototype.then = function (callback) {
  * @return {[type]}          返回animation实例
  */
 Animation.prototype.start = function (interval) {
-    // 如果animation已经是开始状态，则直接返回实例
-    if (this.state === STATE_START) {
-        return this;
-    }
-    // 如果任务链数组 taskQueue 为空（没有任务），
-    // 则直接返回实例
-    if (!this.taskQueue.length) {
-        return this;
-    }
-    // 设置animation状态为 开始状态
-    this.state = STATE_START;
+  // 如果animation已经是开始状态，则直接返回实例
+  if (this.state === STATE_START) {
+    return this
+  }
+  // 如果任务链数组 taskQueue 为空（没有任务），
+  // 则直接返回实例
+  if (!this.taskQueue.length) {
+    return this
+  }
+  // 设置animation状态为 开始状态
+  this.state = STATE_START
 
-    // 将interval 保存到类中
-    this.interval = interval || DEFAULT_INTERVAL;
+  // 将interval 保存到类中
+  this.interval = interval || DEFAULT_INTERVAL
 
-    // 开始执行任务
-    this._runTask();
+  // 开始执行任务
+  this._runTask()
 
-    // 返回实例（链式调用）
-    return this;
+  // 返回实例（链式调用）
+  return this
 }
 
 /**
@@ -219,34 +215,34 @@ Animation.prototype.start = function (interval) {
  * @return {[type]}       返回this._add();
  */
 Animation.prototype.repeat = function (times) {
-    var _this = this;
-    var taskFn = function () {
-        // 没有传入times 时，相当于无线循环
-        if (typeof times === "undefined") {
-            // 无限回退到上一个任务
-            // 这里的 this.index-- 的操作，实现了切换到上一个任务，
-            _this.index--;
-            _this._runTask();
+  var _this = this
+  var taskFn = function () {
+    // 没有传入times 时，相当于无线循环
+    if (typeof times === 'undefined') {
+      // 无限回退到上一个任务
+      // 这里的 this.index-- 的操作，实现了切换到上一个任务，
+      _this.index--
+      _this._runTask()
 
-            // 及时停止，不往下执行
-            return;
-        }
-
-        if (times) {
-            times--;
-            // 回退
-            _this.index--;
-            _this._runTask();
-        }
-        // times为 0 ，（达到了重复的次数）
-        else {
-            var task = _this.taskQueue[_this.index];
-            _this._next(task);
-        }
+      // 及时停止，不往下执行
+      return
     }
-    var type = TASK_SYNC;
 
-    return this._add(taskFn, type);
+    if (times) {
+      times--
+      // 回退
+      _this.index--
+      _this._runTask()
+    }
+    // times为 0 ，（达到了重复的次数）
+    else {
+      var task = _this.taskQueue[_this.index]
+      _this._next(task)
+    }
+  }
+  var type = TASK_SYNC
+
+  return this._add(taskFn, type)
 }
 
 /**
@@ -255,7 +251,7 @@ Animation.prototype.repeat = function (times) {
  * @return {[type]} 返回animation.repeat()
  */
 Animation.prototype.repeatForever = function () {
-    return this.repeat();
+  return this.repeat()
 }
 
 /**
@@ -263,11 +259,10 @@ Animation.prototype.repeatForever = function () {
  * @param  {[type]} time 等待时长
  */
 Animation.prototype.wait = function (time) {
-
-    if (this.taskQueue && this.taskQueue.length > 0) {
-        this.taskQueue[this.taskQueue.length - 1].wait = time;
-    } 
-    return this;
+  if (this.taskQueue && this.taskQueue.length > 0) {
+    this.taskQueue[this.taskQueue.length - 1].wait = time
+  }
+  return this
 }
 
 /**
@@ -275,12 +270,12 @@ Animation.prototype.wait = function (time) {
  * @return {[type]} 返回animation实例
  */
 Animation.prototype.pause = function () {
-    if (this.state === STATE_START) {
-        this.state = STATE_STOP;
-        this.timeline.stop();
-        return this;
-    }
-    return this;
+  if (this.state === STATE_START) {
+    this.state = STATE_STOP
+    this.timeline.stop()
+    return this
+  }
+  return this
 }
 
 /**
@@ -288,12 +283,12 @@ Animation.prototype.pause = function () {
  * @return {[type]} 返回animation实例
  */
 Animation.prototype.resume = function () {
-    if (this.state === STATE_STOP) {
-        this.state = STATE_START;
-        this.timeline.resume();
-        return this;
-    }
-    return this;
+  if (this.state === STATE_STOP) {
+    this.state = STATE_START
+    this.timeline.resume()
+    return this
+  }
+  return this
 }
 
 /**
@@ -301,14 +296,14 @@ Animation.prototype.resume = function () {
  * @return {[type]} 返回animation实例
  */
 Animation.prototype.dispose = function () {
-    if (this.state !== this.STATE_INITIAL) {
-        this.state = STATE_INITIAL;
-        this.taskQueue = null;
-        this.timeline.stop();
-        this.timeline = null;
-        return this;
-    }
-    return this;
+  if (this.state !== STATE_INITIAL) {
+    this.state = STATE_INITIAL
+    this.taskQueue = null
+    this.timeline.stop()
+    this.timeline = null
+    return this
+  }
+  return this
 }
 
 /**
@@ -318,16 +313,15 @@ Animation.prototype.dispose = function () {
  * @param {[type]} type   任务类型
  */
 Animation.prototype._add = function (taskFn, type) {
+  // 往任务队列taskQueue数组中 添加一个任务对象
+  this.taskQueue.push({
+    taskFn: taskFn,
+    type: type,
+  })
 
-    // 往任务队列taskQueue数组中 添加一个任务对象
-    this.taskQueue.push({
-        taskFn: taskFn,
-        type: type
-    });
-
-    // ！！链式调用关键，
-    // 返回Animation 实例，以实现链式调用
-    return this;
+  // ！！链式调用关键，
+  // 返回Animation 实例，以实现链式调用
+  return this
 }
 
 /**
@@ -335,29 +329,29 @@ Animation.prototype._add = function (taskFn, type) {
  * 执行任务
  */
 Animation.prototype._runTask = function () {
-    // 如果任务队列为空 或 animation状态 不为开始状态，
-    // 直接结束
-    if (!this.taskQueue || this.state !== STATE_START) {
-        return;
-    }
-    // 任务执行完毕时，释放资源，并结束
-    if (this.index === this.taskQueue.length) {
-        this.dispose();
-        return;
-    }
+  // 如果任务队列为空 或 animation状态 不为开始状态，
+  // 直接结束
+  if (!this.taskQueue || this.state !== STATE_START) {
+    return
+  }
+  // 任务执行完毕时，释放资源，并结束
+  if (this.index === this.taskQueue.length) {
+    this.dispose()
+    return
+  }
 
-    // 获得任务链上当前任务对象 ({taskFn: taskFn, type: type})
-    var task = this.taskQueue[this.index];
+  // 获得任务链上当前任务对象 ({taskFn: taskFn, type: type})
+  var task = this.taskQueue[this.index]
 
-    // 如果任务类型为同步任务
-    // 执行this._add 方法时，已经将任务类型(type)添加到task中
-    if (task.type === TASK_SYNC) {
-        this._syncTask(task);
-    }
-    // 否则如果任务类型为异步任务
-    else if (task.type === TASK_ASYNC) {
-        this._asyncTask(task);
-    }
+  // 如果任务类型为同步任务
+  // 执行this._add 方法时，已经将任务类型(type)添加到task中
+  if (task.type === TASK_SYNC) {
+    this._syncTask(task)
+  }
+  // 否则如果任务类型为异步任务
+  else if (task.type === TASK_ASYNC) {
+    this._asyncTask(task)
+  }
 }
 
 /**
@@ -367,17 +361,17 @@ Animation.prototype._runTask = function () {
  * @param  {[type]} task 执行的任务对象
  */
 Animation.prototype._syncTask = function (task) {
-    var _this = this;
-    var next = function () {
-        _this._next(task)
-    }
+  var _this = this
+  var next = function () {
+    _this._next(task)
+  }
 
-    var taskFn = task.taskFn;
+  var taskFn = task.taskFn
 
-    // ！！切换到下一个任务，
-    // 将 next 作为参数（回调函数）传入taskFn，
-    // 来实现切换到下一个任务
-    taskFn(next);
+  // ！！切换到下一个任务，
+  // 将 next 作为参数（回调函数）传入taskFn，
+  // 来实现切换到下一个任务
+  taskFn(next)
 }
 
 /**
@@ -388,24 +382,24 @@ Animation.prototype._syncTask = function (task) {
  * @param  {[type]} task 执行的任务对象
  */
 Animation.prototype._asyncTask = function (task) {
-    var _this = this;
-    /**
-     * 定义每一帧执行的回调函数，
-     * Timeline实例里面的 Timeline.onenterFrame
-     * @param  {[type]} time 从动画开始到当前执行的时间
-     */
-    var enterframe = function (time) {
-        var taskFn = task.taskFn;
-        var next = function () {
-            // 停止当前任务
-            _this.timeline.stop();
-            // 执行下一个任务
-            _this._next(task);
-        }
-        taskFn(next, time);
+  var _this = this
+  /**
+   * 定义每一帧执行的回调函数，
+   * Timeline实例里面的 Timeline.onenterFrame
+   * @param  {[type]} time 从动画开始到当前执行的时间
+   */
+  var enterframe = function (time) {
+    var taskFn = task.taskFn
+    var next = function () {
+      // 停止当前任务
+      _this.timeline.stop()
+      // 执行下一个任务
+      _this._next(task)
     }
-    this.timeline.onenterFrame = enterframe;
-    this.timeline.start(this.interval);
+    taskFn(next, time)
+  }
+  this.timeline.onenterFrame = enterframe
+  this.timeline.start(this.interval)
 }
 
 /**
@@ -416,22 +410,22 @@ Animation.prototype._asyncTask = function (task) {
  * @param {[type]} task 当前任务
  */
 Animation.prototype._next = function (task) {
-    var _this = this;
-    // 将任务队列索引值++，同时调用 this._runTask，
-    // 执行到这一步时，任务又回到了this._runTask，
-    // ！！此时，这里的 this.index++ 的操作，实现了切换到下一个任务，
-    // 整个任务链就被串起来了
-    this.index++;
-    
-    if (task.wait) {
-        setTimeout(function(){
-            _this._runTask();
-        }, task.wait);
-    } else {
-        this._runTask();
-    }
+  var _this = this
+  // 将任务队列索引值++，同时调用 this._runTask，
+  // 执行到这一步时，任务又回到了this._runTask，
+  // ！！此时，这里的 this.index++ 的操作，实现了切换到下一个任务，
+  // 整个任务链就被串起来了
+  this.index++
+
+  if (task.wait) {
+    setTimeout(function () {
+      _this._runTask()
+    }, task.wait)
+  } else {
+    this._runTask()
+  }
 }
 
-module.exports = function() {
-    return new Animation();
+module.exports = function () {
+  return new Animation()
 }
--- src/timeline.js
+++ src/timeline.js
@@ -1,110 +1,110 @@
-'use strict';
+'use strict'
 
 // 默认执行的时间 间隔（每秒60帧 17ms 左右）
-var DEFAULT_INTERVAL = 1000 / 60;
+var DEFAULT_INTERVAL = 1000 / 60
 
 /**
  * Timeline 状态定义
  */
 // 初始状态
-var STATE_INITIAL = 0;
+var STATE_INITIAL = 0
 // 开始状态
-var STATE_START = 1;
+var STATE_START = 1
 // 停止状态
-var STATE_STOP = 2;
-
+var STATE_STOP = 2
 
 /**
  * 定义 requestAnimationFrame 方法，实现兼容操作
  */
-var requestAnimationFrame = (function(){
-    return window.requestAnimationFrame || 
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        function (callback) {
-            return window.setTimeout(callback, callback.interval || DEFAULT_INTERVAL);
-        };
-})();
+var requestAnimationFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, callback.interval || DEFAULT_INTERVAL)
+    }
+  )
+})()
 
 /**
- * 清除 定时器 requestAnimationFrame 
+ * 清除 定时器 requestAnimationFrame
  */
-var cancelRequestAnimationFrame = (function(){
-    return window.cancelAnimationFrame ||
-        window.webkitCancelRequestAnimationFrame ||
-        window.mozCancelRequestAnimationFrame ||
-        window.oCanccelRequestAnimationFrame ||
-        function (timerId) {
-            return window.clearTimeout(timerId);
-        };
-})();
+var cancelRequestAnimationFrame = (function () {
+  return (
+    window.cancelAnimationFrame ||
+    window.webkitCancelRequestAnimationFrame ||
+    window.mozCancelRequestAnimationFrame ||
+    window.oCanccelRequestAnimationFrame ||
+    function (timerId) {
+      return window.clearTimeout(timerId)
+    }
+  )
+})()
 
 /**
  * Timeline 时间轴类
  */
-function Timeline () {
-    this.animationHandler = 0;
-    this.state = STATE_INITIAL;
+function Timeline() {
+  this.animationHandler = 0
+  this.state = STATE_INITIAL
 }
 
 /**
  * 时间轴上每一次回调执行的函数
  * @param  {[type]} time 从动画开始到当前执行的时间
  */
-Timeline.prototype.onenterFrame = function (time) {
-
-}
+Timeline.prototype.onenterFrame = function (time) {}
 
 /**
  * 让动画开始
  * @param  {[type]} interval 每一次回调它的间隔时间
  */
 Timeline.prototype.start = function (interval) {
+  if (this.state === STATE_START) {
+    return
+  }
+  this.state = STATE_START
+  this.interval = interval || DEFAULT_INTERVAL
 
-    if (this.state === STATE_START) {
-        return;
-    }
-    this.state = STATE_START;
-    this.interval = interval || DEFAULT_INTERVAL;
-
-    // 开始this.start 方法，并传入当前时间
-    // +new Date() 相当于 new Date().getTime();
-    startTimeline(this, +new Date());
+  // 开始this.start 方法，并传入当前时间
+  // +new Date() 相当于 new Date().getTime();
+  startTimeline(this, +new Date())
 }
 
 /**
  * 让动画停止
  */
 Timeline.prototype.stop = function () {
-    if (this.state !== STATE_START) {
-        return;
-    }
-    this.state = STATE_STOP;
+  if (this.state !== STATE_START) {
+    return
+  }
+  this.state = STATE_STOP
 
-    // 如果动画开始过（即有记录startTime的情况下），
-    // 则记录动画从开始到现在所经历的时间(this.dur)
-    if (this.startTime) {
-        this.dur = +new Date() - this.startTime;
-    }
-    cancelRequestAnimationFrame(this.animationHandler);
+  // 如果动画开始过（即有记录startTime的情况下），
+  // 则记录动画从开始到现在所经历的时间(this.dur)
+  if (this.startTime) {
+    this.dur = +new Date() - this.startTime
+  }
+  cancelRequestAnimationFrame(this.animationHandler)
 }
 
 /**
  * 暂停的动画重新开始
  */
 Timeline.prototype.resume = function () {
-    if (this.state === STATE_START) {
-        return;
-    }
-    if (!this.dur || !this.interval) {
-        return;
-    }
+  if (this.state === STATE_START) {
+    return
+  }
+  if (!this.dur || !this.interval) {
+    return
+  }
 
-    this.state = STATE_START;
+  this.state = STATE_START
 
-    // ！！去无缝连接动画，（好好理解下流程情况就懂了，很难备注）
-    startTimeline(this, +new Date() - this.dur);
+  // ！！去无缝连接动画，（好好理解下流程情况就懂了，很难备注）
+  startTimeline(this, +new Date() - this.dur)
 }
 
 /**
@@ -112,33 +112,32 @@ Timeline.prototype.resume = function () {
  * @param  {[type]} timeline  时间轴实例
  * @param  {[type]} startTime 动画开始的时间戳
  */
-function startTimeline (timeline, startTime) {
+function startTimeline(timeline, startTime) {
+  // 记录上一次回调的时间戳
+  var lastTick = +new Date()
 
-    // 记录上一次回调的时间戳
-    var lastTick = +new Date();
+  // 对timeline 的 startTime 进行记录
+  timeline.startTime = startTime
+  nextTick.interval = timeline.interval
 
-    // 对timeline 的 startTime 进行记录
-    timeline.startTime = startTime;
-    nextTick.interval = timeline.interval;
+  nextTick()
 
-    nextTick();
+  // 对requestAnimationFrame 进行封装
+  // 定义每一帧执行的函数
+  function nextTick() {
+    var now = +new Date()
 
-    // 对requestAnimationFrame 进行封装
-    // 定义每一帧执行的函数
-    function nextTick () {
-        var now = +new Date();
+    // 每一帧(17ms)执行的结果都用 timeline.animationHandler 保存
+    timeline.animationHandler = requestAnimationFrame(nextTick)
 
-        // 每一帧(17ms)执行的结果都用 timeline.animationHandler 保存
-        timeline.animationHandler = requestAnimationFrame(nextTick);
-        
-        // ！！如果 当前时间(now)减去 上一次回调执行的时间(lastTick)，
-        // 大于等于 设置的时间间隔(interval)时，
-        // 则 timeline.onenterFrame 可以被执行
-        if (now - lastTick >= timeline.interval) {
-            timeline.onenterFrame(now - startTime);
-            lastTick = now;
-        }
+    // ！！如果 当前时间(now)减去 上一次回调执行的时间(lastTick)，
+    // 大于等于 设置的时间间隔(interval)时，
+    // 则 timeline.onenterFrame 可以被执行
+    if (now - lastTick >= timeline.interval) {
+      timeline.onenterFrame(now - startTime)
+      lastTick = now
     }
+  }
 }
 
-module.exports = Timeline;
+module.exports = Timeline
